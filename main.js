f6ca4503b421f19c4ddaa7927f0f8026d3806e5f
ASSEKO <assekoazareel222@gmail.com>
2024-09-05 17:21:36 +0100

    deuxieme commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let form = document.getElementById("sheetdb-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("sheetdb-form")),
+  })
+    .then((response) => response.json())
+    .then((html) => {
+      // you can put any JS code here
+      window.open("https://hismusicinstitute.com/formation/", "_blank");
+    });
+});
